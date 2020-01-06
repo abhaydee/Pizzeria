1afe8337fcfe17053c0f42102137d5a4842ede53
abhaydee <abhaygirish1997@gmail.com>
2020-01-06 23:02:56 +0530

    written test for pizza data service

--- src/app/pizzadata.service.spec.ts
+++ src/app/pizzadata.service.spec.ts
@@ -1,12 +1,64 @@
 import { TestBed } from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
 
 import { PizzadataService } from './pizzadata.service';
+import { request } from 'http';
 
 describe('PizzadataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service:PizzadataService;
+  let httpMock;
+  let a,b,c,d:any;
+  let id:any
+  beforeEach(() => {TestBed.configureTestingModule({
+    imports:[HttpClientTestingModule],
+    providers:[PizzadataService]
+  })
+  service = TestBed.get(PizzadataService);
+  httpMock = TestBed.get(HttpTestingController);
 
+});
   it('should be created', () => {
     const service: PizzadataService = TestBed.get(PizzadataService);
     expect(service).toBeTruthy();
   });
+
+
+it('should get pizza data',()=>{
+  service.getpizzadetails().subscribe((pizza)=>{
+    expect(pizza).toBeTruthy();
+    
+  });
+  const req=httpMock.expectOne("http://localhost:5000/getpizza")
+  expect(req.request.method).toBe("GET")
+})
+
+it('should post pizza data',()=>{
+  service.postpizzadetails(a,b,c,d).subscribe((details)=>{
+    expect(details).toBeTruthy();
+
+  })
+  const req1=httpMock.expectOne("http://localhost:5000/postpizza")
+  expect(req1.request.method).toBe("POST");
+})
+
+it('delete the pizza data',()=>{
+  service.senddeletedata(id).subscribe((a)=>{
+    expect(a).toBeTruthy();
+  })
+  const req2=httpMock.expectOne("http://localhost:5000/deletingdata");
+  expect(req2.request.method).toBe("POST")
+})
+
+it('send temp pizza data',()=>{
+  service.gettempdata().subscribe((temp)=>{
+    expect(temp).toBeTruthy();
+  });
+  const req3=httpMock.expectOne("http://localhost:5000/getdata")
+  expect(req3.request.method).toBe("GET")
+})
+
+
+
+
+
 });
