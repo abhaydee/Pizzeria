531178d265598ff577613e736a395b4d6304ef07
abhaydee <abhaygirish1997@gmail.com>
2020-01-07 12:56:13 +0530

    added e2e testing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, element, by } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -7,11 +7,42 @@ describe('workspace-project App', () => {
   beforeEach(() => {
     page = new AppPage();
   });
-
-  it('should display welcome message', () => {
+  it('should check our story',()=>{
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('finalsimulation app is running!');
+    let c=element.all(by.tagName('h3')).first();
+    let t=c.getText()
+    expect(t).toEqual('Our Story');
   });
+
+  it( 'should check for button text', () =>{
+    page.navigateOrder();
+    let pl=element(by.css('.testing'));
+    let text=pl.getText();
+    expect(text).toEqual('Add to cart');
+});
+
+
+it( 'should check for ingredients text', () =>{
+  page.navigateCustom();
+  let pl=element.all(by.css('#paragraph'));
+  let text=pl.getText();
+  expect(text).toContain('Total Cost');
+});
+
+  it('should check nav',()=>{
+    page.navigateTo();
+    let c=element(by.tagName('nav')).isPresent();
+    expect(c).toBeTruthy();
+  });
+
+
+  it('should check Pizzaria in navbar',()=>{
+    page.navigateTo();
+    let c=element.all(by.tagName('a')).first();
+    let t=c.getText()
+    expect(t).toEqual('Pizzeria');
+  });
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
@@ -20,4 +51,13 @@ describe('workspace-project App', () => {
       level: logging.Level.SEVERE,
     } as logging.Entry));
   });
+//   it( 'should check for paragraf text', () =>{
+//     page.navigateTo();
+//     let pl=element(by.css('.greenc'));
+//     let text=pl.getText();
+//     expect(text).toEqual('This is paral');
+// });
+
+
+
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,6 +5,18 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
+  navigateOrder(){
+    let url="http://localhost:4200/orders"
+    return browser.get(url) as Promise<any>;
+
+  }
+
+  navigateCustom(){
+    let url="http://localhost:4200/ingredients"
+    return browser.get(url) as Promise<any>;
+
+  }
+
   getTitleText() {
     return element(by.css('app-root .content span')).getText() as Promise<string>;
   }
